ebf6b48293fb1ca5e8c4f1d6de5c84d495eedcbc
DanBrown47 <danwand47@gmail.com>
2021-05-06 13:06:40 +0530

    Cards

---
+++ index.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+const { red, white, green, cyan, bold, blue } = require('colorette')
+const head = `${blue(bold('  Danwand NS') + ' /')} ${blue('DanBrown47')} / ${white('Rooter')}` 
+const info = {
+
+	Email: green('danwand47@gmail.com') + '\n',
+	Work: white(`${red('Wattlecorp CyberSecurity Labs LLP')}, AI Researcher`),
+  	Github: cyan('DanBrown47'),
+  	Twitter: cyan('@danwand47'),
+  	LinkedIn: cyan('linkedin.com/in/danwand') + '\n',
+  	Card: white('npx danwand')
+}
+
+
+
+const body = Object.entries(info)
+  .map(([key, value]) => [white(bold(`${key}:`.padStart(16, ' '))), value].join(' '))
+  .join('\n')
+
+console.log('\n' + [head, body].join('\n\n') + '\n')
